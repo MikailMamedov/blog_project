b428b71a8fd9ef512710cc42b03357543d976434
MikailMamedov <94541297+MikailMamedov@users.noreply.github.com>
2024-11-12 21:06:33 +0400

    branch: pushing new branch to repo

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()  # сохраняем пользователя
-            login(request, user)  # автоматически логиним после регистрации
-            return redirect('index')  # перенаправление на главную страницу после успешной регистрации
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('index')
+        else:
+            messages.error(request, 'Registration failed. Please correct the errors.')
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+
+
 
