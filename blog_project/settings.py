976869f0006d8fe325e95e7f7c253b2da9f17947
MikailMamedov <94541297+MikailMamedov@users.noreply.github.com>
2025-01-31 12:42:26 +0400

    feat: update .gitignore to exclude venv and improve environment management; add Whitenoise package files for static file handling

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+# Инициализация django-environ
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-magdx*b&d670za%-wqi@2=!$8j=mzia^pv)@%sv(lu9($lfn8k'
+# SECRET_KEY = 'django-insecure-magdx*b&d670za%-wqi@2=!$8j=mzia^pv)@%sv(lu9($lfn8k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
