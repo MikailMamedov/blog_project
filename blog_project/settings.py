8b59ae227c0b244764258d1091419fb19c2d20ab
MikailMamedov <94541297+MikailMamedov@users.noreply.github.com>
2025-09-18 02:16:19 +0400

    refactor: remove trailing whitespace in settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -55,7 +55,7 @@ AUTHENTICATION_BACKENDS = (
     # 'social_core.backends.twitter.TwitterOAuth',
     'django.contrib.auth.backends.ModelBackend',
 )
-
+ 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
